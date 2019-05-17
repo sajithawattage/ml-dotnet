b010ea995e745357e426ec35b7d070992992c542
sajithawattage <sajitha.wattage@gmail.com>
2019-05-18 00:35:26 +0800

    initial commit

---
+++ MachineLearning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MachineLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
